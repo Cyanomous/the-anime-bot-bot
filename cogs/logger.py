7d45d2f1eea0f91d9952c2c059321b4a63fff196
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-02-18 14:06:11 -0800

    Update logger.py

--- cogs/logger.py
+++ cogs/logger.py
@@ -1,11 +1,15 @@
 import discord
 from discord.ext import commands
 
+
 class logger(commands.Cog):
-  def __init__(self, bot):
-    self.bot = bot
-  @commands.Cog.listener()
-  async def on_socket_response(self, msg):
-    pass
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_socket_response(self, msg):
+        pass
+
+
 def setup(bot):
-  bot.add_cog(logger(bot))
+    bot.add_cog(logger(bot))

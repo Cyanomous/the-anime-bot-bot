68371344a1e7174107287eb5102de2510b229919
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-02-18 14:12:49 -0800

    Update main.py

--- main.py
+++ main.py
@@ -17,13 +17,23 @@ import discord
 from utils.subclasses import AnimeBot
 from discord.ext import commands
 import functools
+
+
 bot = AnimeBot()
-os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True" 
+
+
+os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True"
 os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
+
+
 @bot.check
 async def check(ctx):
-  return not ctx.author.id == 694296778106863636
+    return not ctx.author.id == 694296778106863636
+
+
 bot.remove_command("help")
+
+
 @bot.command(name="help")
 async def _help(ctx, *, command: str = None):
     """Shows help about a command or the bot"""
@@ -35,11 +45,16 @@ async def _help(ctx, *, command: str = None):
 
             if entity is None:
                 clean = command.replace('@', '@\u200b')
-                failed_command = re.match(f"^({ctx.prefix})\s*(.*)", f"ovo {clean}", flags=re.IGNORECASE).group(2)
-                matches = difflib.get_close_matches(failed_command, ctx.bot.command_list)
+                failed_command = re.match(f"^({ctx.prefix})\s*(.*)",
+                                          f"ovo {clean}",
+                                          flags=re.IGNORECASE).group(2)
+                matches = difflib.get_close_matches(failed_command,
+                                                    ctx.bot.command_list)
                 if not matches:
                     return
-                return await ctx.send(f"Command or category '{clean}' not found. Did you mean `{matches[0]}`?")
+                return await ctx.send(
+                    f"Command or category '{clean}' not found. Did you mean `{matches[0]}`?"
+                )
             elif isinstance(entity, commands.Command):
                 p = await HelpPaginator.from_command(ctx, entity)
             else:
@@ -47,11 +62,17 @@ async def _help(ctx, *, command: str = None):
 
         await p.paginate()
     except Exception as error:
-      print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-      traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
-      await ctx.send(error)
+        print('Ignoring exception in command {}:'.format(ctx.command),
+              file=sys.stderr)
+        traceback.print_exception(type(error),
+                                  error,
+                                  error.__traceback__,
+                                  file=sys.stderr)
+        await ctx.send(error)
+
+
 for file in os.listdir("./cogs"):
-  if file.endswith(".py"):
-    bot.load_extension(f"cogs.{file[:-3]}")
+    if file.endswith(".py"):
+        bot.load_extension(f"cogs.{file[:-3]}")
 
 bot.run(TOKEN)

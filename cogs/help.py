c2a33de53901ce527eecded66437f7088341f2c6
alvinzzzzz <64497526+alvinzzzzz@users.noreply.github.com>
2021-02-20 22:05:01 -0800

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -2,18 +2,21 @@ import discord
 from discord.ext import commands
 
 from jishaku.paginators import PaginatorEmbedInterface
+from jishaku.models import copy_context_with
 
 
 class HelpCommand(commands.HelpCommand):
     def get_command_signature(self, command):
         return '%s%s %s' % (self.clean_prefix, command.qualified_name, command.signature)
-        
+
     async def send_group_help(self, group):
-        embed = discord.Embed(color=self.context.bot.color, title=f"{self.clean_prefix}{group.qualified_name}", description=group.short_doc or "oh seems like my owner is too lazy to add help for this command sorry")
+        embed = discord.Embed(color=self.context.bot.color, title=f"{self.clean_prefix}{group.qualified_name}",
+                              description=group.short_doc or "oh seems like my owner is too lazy to add help for this command sorry")
         await self.context.send(embed=embed)
 
     async def send_command_help(self, command):
-        embed = discord.Embed(color=self.context.bot.color, title=f"{self.clean_prefix}{command.qualified_name}", description=command.short_doc or "oh seems like my owner is too lazy to add help for this command sorry")
+        embed = discord.Embed(color=self.context.bot.color, title=f"{self.clean_prefix}{command.qualified_name}",
+                              description=command.short_doc or "oh seems like my owner is too lazy to add help for this command sorry")
         await self.context.send(embed=embed)
 
     async def send_cog_help(self, cog):
@@ -22,7 +25,8 @@ class HelpCommand(commands.HelpCommand):
         commands_ = [self.get_command_signature(i) for i in commands_]
         for i in commands_:
             paginator.add_line(i)
-        interface = PaginatorEmbedInterface(self.context.bot, paginator, owner=self.contextauthor)
+        interface = PaginatorEmbedInterface(
+            self.context.bot, paginator, owner=self.contextauthor)
         await interface.send_to(self.context)
 
     async def send_bot_help(self, mapping):
@@ -36,7 +40,8 @@ class HelpCommand(commands.HelpCommand):
             597589960270544916: "pictures",
             744346239075877518: "utility"
         }
-        embed = discord.Embed(color=self.context.bot.color, description="**Hi welcome to The Anime Bot's help menu**\n```diff\nBefore we start something you need to know about\n <argument> means that argument is require\n[requirment] mean that argument is optional\n```")
+        embed = discord.Embed(color=self.context.bot.color,
+                              description="**Hi welcome to The Anime Bot's help menu**\n```diff\nBefore we start something you need to know about\n <argument> means that argument is require\n[requirment] mean that argument is optional\n```")
         embed.add_field(name="Categorys", value="**Animes:** <:rooDuck:739614767941156874>\n**Fun:** <:rooAww:747680003021471825>\n**Moderations:** <:rooCop:596577110982918146>\n**Music:** <a:rooCool:747680120763973654>\n**Others:** <a:rooClap:759933903959228446>\n**Pictures:** <:rooBless:597589960270544916>\n**Utility:** <a:rooLove:744346239075877518>")
         channel = self.get_destination()
         message = await channel.send(embed=embed)
@@ -46,12 +51,15 @@ class HelpCommand(commands.HelpCommand):
             def check(payload):
                 return payload.message_id == message.id and payload.author_id == self.context.author.id and payload.emoji.id in dicts.keys()
             payload = await self.context.bot.wait_for("raw_reaction_add", check=check)
+            await self.context.bot.invoke(copy_context_with(self.context, author=self.context.author, content=ctx.prefix + dicts.get(payload.emoji.id)))
             await self.send_cog_help(self.context.bot.get_cog(dicts.get(payload.emoji.id)))
+
 
 class help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         bot.help_command = HelpCommand()
-    
+
+
 def setup(bot):
     bot.add_cog(help(bot))

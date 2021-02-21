01fa7133017319ed5ecb231be5bfda003c425855
alvinzzzzz <64497526+alvinzzzzz@users.noreply.github.com>
2021-02-20 22:36:40 -0800

    recent changed in info

--- cogs/others.py
+++ cogs/others.py
@@ -383,7 +383,7 @@ class others(commands.Cog):
         repo = g.get_repo("Cryptex-github/the-anime-bot-bot").get_commits()[:3]
         lists = []
         for i in repo:
-            lists.append(f"`[{i.commit.sha}]({i.commit.url})` {i.commit.message}")
+            lists.append(f"[{i.commit.sha[5]}]({i.commit.url}) {i.commit.message}")
         embed.add_field(name="Recent changes", value="\n".join(lists))
         embed.add_field(
             name=" stats ",

8f3ec06b67aa67f0296574f7138220e893e1bf91
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-02-22 17:38:37 -0800

    o

--- utils/utils.py
+++ utils/utils.py
@@ -1,22 +1,22 @@
 import discord
 from discord.ext import commands
 
-class utils:
-    async def get_pic(self, ctx):
-        msg = ctx.message
-        if not msg.reference:
-            return False
-        if msg.reference.cached_message:
-            if msg.reference.cached_message.attachments:
-                return msg.reference.cached_message.attachments.url
-            if msg.reference.cached_message.embeds.url:
-                return msg.reference.cached_message.embeds.url
-        else:
-            if msg.reference.cached_message.message_id and msg.reference.cached_message.channel_id:
-                msg = await ctx.bot.get_channel(msg.channel_id).fetch_message(msg.message.id)
-                if msg.attachments:
-                    return msg.reference.cached_message.attachments.url
-                if msg.embeds.url:
-                    return msg.reference.cached_message.embeds.url
 
+async def get_pic(self, ctx):
+    msg = ctx.message
+    if not msg.reference:
         return False
+    if msg.reference.cached_message:
+        if msg.reference.cached_message.attachments:
+            return msg.reference.cached_message.attachments.url
+        if msg.reference.cached_message.embeds.url:
+            return msg.reference.cached_message.embeds.url
+    else:
+        if msg.reference.cached_message.message_id and msg.reference.cached_message.channel_id:
+            msg = await ctx.bot.get_channel(msg.channel_id).fetch_message(msg.message.id)
+            if msg.attachments:
+                return msg.reference.cached_message.attachments.url
+            if msg.embeds.url:
+                return msg.reference.cached_message.embeds.url
+
+    return False

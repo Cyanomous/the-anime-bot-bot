9e623c28e8f830000a09ad38baca7c396fd2b957
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-02-25 11:07:01 -0800

    close the buffer

--- cogs/utility.py
+++ cogs/utility.py
@@ -244,8 +244,7 @@ class utility(commands.Cog):
         cache = {}
         for key, page in page_types.items():
             sub = cache[key] = {}
-            async with self.bot.session.get(page +
-                                                   '/objects.inv') as resp:
+            async with self.bot.session.geta(page + '/objects.inv') as resp:
                 if resp.status != 200:
                     raise RuntimeError(
                         'Cannot build rtfm lookup table, try again later.')
@@ -488,7 +487,11 @@ class utility(commands.Cog):
     @staticmethod
     @asyncexe()
     def txt_(thing):
-        return discord.File(BytesIO(thing.encode("utf-8")), "something.txt")
+        buffer = BytesIO(thing.encode("utf-8"))
+        try:
+            return discord.File(buffer, "something.txt")
+        finally:
+            buffer.close()
     @commands.command()
     async def txt(self, ctx, *, anything:str=None):
         if anything.startswith("https://mystb.in/"):

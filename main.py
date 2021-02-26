9a4bf5d07377b10f3ff0e18f414428268fb98a02
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-02-25 18:11:42 -0800

    hm

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from utils.subclasses import AnimeBot
 logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)

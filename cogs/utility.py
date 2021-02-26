36cf80017c4890e30f0fc4756a606c5ed061024c
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-02-25 16:12:09 -0800

    smh

--- cogs/utility.py
+++ cogs/utility.py
@@ -442,8 +442,8 @@ class utility(commands.Cog):
                           to_lang=to_lang).translate(thing)
 
     @commands.command()
-    async def ip(self, ctx, ip:int):
-        async with self.bot.session.get("https://api.ksoft.si/kumo/geoip", headers = {"Authorization": os.getenv("ksoft")}, params = {"ip": str(ip)}) as resp:
+    async def ip(self, ctx, ip):
+        async with self.bot.session.get("https://api.ksoft.si/kumo/geoip", headers = {"Authorization": os.getenv("ksoft")}, params = {"ip": ip}) as resp:
             res = await resp.json()
             res.get("datas").pop("api")
             await ctx.send(json.dumps(res, indent=4))
